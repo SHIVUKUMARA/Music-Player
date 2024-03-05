d9f0a8fbb007b61c4775b2862763ca7e98a308d3
SHIVUKUMARA <130156525+SHIVUKUMARA@users.noreply.github.com>
2024-03-05 12:32:23 +0530

    Add files via upload

---
+++ backend/routes/song.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const Song = require("../models/Song");
+const User = require("../models/user");
+
+router.post(
+  "/create",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { name, thumbnail, track } = req.body;
+    if (!name || !thumbnail || !track) {
+      return res
+        .status(301)
+        .json({ error: "Insufficient details to create song." });
+    }
+    const artist = req.user._id;
+    const songDetails = { name, thumbnail, track, artist };
+    const createdSong = await Song.create(songDetails);
+    return res.status(200).json(createdSong);
+  }
+);
+
+// get route to get all songs i have published
+router.get(
+  "/get/mysongs",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const songs = await Song.find({ artist: req.user._id }).populate("artist");
+    return res.status(200).json({ data: songs });
+  }
+);
+
+// get all songs published by perticular artist
+router.get(
+  "/get/artist/:artistId",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { artistId } = req.params;
+
+    const artist = await User.findOne({ _id: artistId });
+    if (!artist) {
+      return res.status(301).json({ error: "Artist not found!" });
+    }
+
+    const songs = await Song.find({ artist: artistId });
+    return res.status(200).json({ data: songs });
+  }
+);
+
+// Get route to get a single song by name
+router.get(
+  "/get/songname/:songName",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { songName } = req.params;
+    const songs = await Song.find({ name: songName }).populate("artist");
+    return res.status(200).json({ data: songs });
+  }
+);
+
+module.exports = router;
