753c24e28694de25f5f81ca70aa067b5660ea546
SHIVUKUMARA <130156525+SHIVUKUMARA@users.noreply.github.com>
2024-03-05 10:16:21 +0530

    Update song.js

--- routes/song.js
+++ routes/song.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Song = require("../models/Song");
-const User = require("../models/user");
+const User = require("../models/User");
 
 router.post(
   "/create",
