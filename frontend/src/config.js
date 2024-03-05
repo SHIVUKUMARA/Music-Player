ae777d574d0d9601cf13452dea8e375f906d74e1
SHIVUKUMARA <130156525+SHIVUKUMARA@users.noreply.github.com>
2024-03-05 12:30:21 +0530

    Add files via upload

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+export const cloudinary_upload_preset = "dlgctv8t";
+
+export const cloudinary_cloud_name = "dcqq10m1j";
---
+++ frontend/src/utils/config.js
@@ -0,0 +1,3 @@
+export const backendUrl = "http://localhost:8000";
+// const backendUrl = ""; // Empty string for relative path if both frontend and backend are running on same port use full while deploying
+// export { backendUrl }
