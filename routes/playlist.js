fd92263506a25bcf73a2a09b3299fc799693be89
SHIVUKUMARA <130156525+SHIVUKUMARA@users.noreply.github.com>
2024-03-05 10:16:03 +0530

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const Playlist = require("../models/playlist");
-const User = require("../models/user");
+const User = require("../models/User");
 const Song = require("../models/Song");
 
 const router = express.Router();
